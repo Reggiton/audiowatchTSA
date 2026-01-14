287b03c29f27f62f26b902204fd21fe69f1dd9cd
Reggiton <grumpyman1024@gmail.com>
2026-01-14 18:16:00 -0500

    Add pages configuration for routing

---
+++ src/pages.config.js
@@ -0,0 +1,16 @@
+import History from './pages/History'
+import Home from './pages/Home'
+import Settings from './pages/Settings'
+import SoundLibrary from './pages/SoundLibrary'
+
+export const PAGES = {
+    "History": History,
+    "Home": Home,
+    "Settings": Settings,
+    "SoundLibrary": SoundLibrary,
+}
+
+export const pagesConfig = {
+    mainPage: "Home",
+    Pages: PAGES,
+}
