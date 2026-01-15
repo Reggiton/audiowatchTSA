0fd1d17cafc6b95dabd412f6b8ef34082cbf2d12
Programing-Penguin <87450738+Programing-Penguin@users.noreply.github.com>
2026-01-15 17:05:04 -0500

    Update table.jsx

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -2,78 +2,109 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+// Wrapper table component with horizontal scroll support
 const Table = React.forwardRef(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
+      // Base table styling
       className={cn("w-full caption-bottom text-sm", className)}
-      {...props} />
+      {...props}
+    />
   </div>
 ))
 Table.displayName = "Table"
 
+// Table header section (<thead>)
 const TableHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    // Adds bottom border to header rows
+    className={cn("[&_tr]:border-b", className)}
+    {...props}
+  />
 ))
 TableHeader.displayName = "TableHeader"
 
+// Table body section (<tbody>)
 const TableBody = React.forwardRef(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
+    // Removes border from last row
     className={cn("[&_tr:last-child]:border-0", className)}
-    {...props} />
+    {...props}
+  />
 ))
 TableBody.displayName = "TableBody"
 
+// Table footer section (<tfoot>)
 const TableFooter = React.forwardRef(({ className, ...props }, ref) => (
   <tfoot
     ref={ref}
-    className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)}
-    {...props} />
+    // Muted background and top border for footer
+    className={cn(
+      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      className
+    )}
+    {...props}
+  />
 ))
 TableFooter.displayName = "TableFooter"
 
+// Table row (<tr>)
 const TableRow = React.forwardRef(({ className, ...props }, ref) => (
   <tr
     ref={ref}
+    // Hover and selected row styles
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props} />
+    {...props}
+  />
 ))
 TableRow.displayName = "TableRow"
 
+// Table header cell (<th>)
 const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
+    // Header cell styling and checkbox alignment
     className={cn(
       "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
-    {...props} />
+    {...props}
+  />
 ))
 TableHead.displayName = "TableHead"
 
+// Table data cell (<td>)
 const TableCell = React.forwardRef(({ className, ...props }, ref) => (
   <td
     ref={ref}
+    // Cell padding and checkbox alignment
     className={cn(
       "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
-    {...props} />
+    {...props}
+  />
 ))
 TableCell.displayName = "TableCell"
 
+// Table caption (<caption>)
 const TableCaption = React.forwardRef(({ className, ...props }, ref) => (
   <caption
     ref={ref}
+    // Caption text below the table
     className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props} />
+    {...props}
+  />
 ))
 TableCaption.displayName = "TableCaption"
 
+// Export all table-related components
 export {
   Table,
   TableHeader,
