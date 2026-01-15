440dc0c93888788d4ab5fac08f7385204cf869b3
Programing-Penguin <87450738+Programing-Penguin@users.noreply.github.com>
2026-01-15 16:39:06 -0500

    Update label.jsx

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,16 +1,24 @@
+// React import
 import * as React from "react"
+
+// Radix label primitive
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva } from "class-variance-authority";
+
+// Used for variant-based classes
+import { cva } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+// Label styles
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
+// Label component
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
 ))
 Label.displayName = LabelPrimitive.Root.displayName
 
+// Export
 export { Label }
