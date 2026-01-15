dabb66632f8fa8bc984d69f912b6f1b0d7aaca9e
Programing-Penguin <87450738+Programing-Penguin@users.noreply.github.com>
2026-01-15 16:56:08 -0500

    Update separator.jsx

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,23 +1,33 @@
+// React import
 import * as React from "react"
+
+// Radix separator primitive
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
+// Utility for conditional class names
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef((
-  { className, orientation = "horizontal", decorative = true, ...props },
-  ref
-) => (
-  <SeparatorPrimitive.Root
-    ref={ref}
-    decorative={decorative}
-    orientation={orientation}
-    className={cn(
-      "shrink-0 bg-border",
-      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-      className
-    )}
-    {...props} />
-))
+// Separator component (horizontal or vertical)
+const Separator = React.forwardRef(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative} // Accessibility: marks separator as decorative
+      orientation={orientation} // horizontal | vertical
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "horizontal"
+          ? "h-[1px] w-full"
+          : "h-full w-[1px]",
+        className
+      )}
+      {...props} />
+  )
+)
 Separator.displayName = SeparatorPrimitive.Root.displayName
 
+// Export
 export { Separator }
