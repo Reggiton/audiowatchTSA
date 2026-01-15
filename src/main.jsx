f5b23a39c7d74365c12cc5abd6c0344a284b78a8
Reggiton <grumpyman1024@gmail.com>
2026-01-15 17:24:12 -0500

    Added comments for readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
+/**
+ * Application Entry Point
+ * Initializes and renders the React app to the DOM
+ * Enables React StrictMode for development checks
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Create React root and render app with StrictMode enabled
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
