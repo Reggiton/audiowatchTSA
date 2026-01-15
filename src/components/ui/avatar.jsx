ddf24e802421ef5cec8b600ce16a299029e5d494
Programing-Penguin <87450738+Programing-Penguin@users.noreply.github.com>
2026-01-15 16:07:46 -0500

    Update avatar.jsx

--- src/components/ui/avatar.jsx
+++ src/components/ui/avatar.jsx
@@ -1,26 +1,31 @@
-"use client"
+"use client" // Client component
 
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
+// Avatar wrapper
 const Avatar = React.forwardRef(({ className, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
     className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
     {...props} />
 ))
+
 Avatar.displayName = AvatarPrimitive.Root.displayName
 
+// Avatar image
 const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
     className={cn("aspect-square h-full w-full", className)}
     {...props} />
 ))
+
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
+// Avatar fallback
 const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
@@ -30,6 +35,7 @@ const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
     )}
     {...props} />
 ))
+
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
 export { Avatar, AvatarImage, AvatarFallback }
